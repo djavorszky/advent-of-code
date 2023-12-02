1d370323ccad67b31d28fa0d94c867993b78bb3c
Daniel Javorszky <daniel.javorszky@gmail.com>
2023-12-02 19:44:49 +0100

    Minor cleanup

--- src/main/days/day2.clj
+++ src/main/days/day2.clj
@@ -48,8 +48,6 @@
        (map #(reduce * (vals %)))
        (reduce +)))
 
-
-
 (def input (slurp "input/2.txt"))
 
 (task1 input)
@@ -58,19 +56,18 @@
 (comment
   (def test-game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
 
-  (count-cubes test-game-imp-2)
 
   (def test-game-imp-1 "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")
   (def test-game-imp-2 "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")
 
   (def res (count-cubes test-game-imp-1))
 
+  (count-cubes test-game-imp-2)
+
   (reduce + (vals res))
 
   (->> (str/split "3 blue, 4 red" #", ")
        (map parse-cubes))
-
-  (second cubes)
 
   (->> (str/split-lines test-input)
        (map count-cubes)
@@ -81,40 +78,16 @@
   
 
 
-  (let [input (str/split test-game #": ")
-        games (second input)
-        id (get-id (first input))
-        sets (str/split games #"; ")]
-    id)
-
-
   (def test-input "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
 
-  (defn enough? [cubes]
-    (and
-     (>= 12 (get cubes "red" 0))
-     (>= 13 (get cubes "green" 0))
-     (>= 14 (get cubes "blue" 0))))
+  (task1 test-input)
 
+  (task2 test-input)
 
-  (->> (second cubes)
-       (apply merge-with max))
-
-  (def cubes (count-cubes test-game))
-
-  (every? true? (map enough? (second cubes)))
-
-  (let [input (str/split test-game-imp-2 #": ")
-        games (second input)
-        id (get-id (first input))
-        sets (str/split games #"; ")]
-    [(Integer. id) (->> sets
-                        (map parse-set)
-                        (mapv list->map))])
 
 
   )
