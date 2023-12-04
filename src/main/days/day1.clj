b3d7ff33f35f1a1b7b9439fe9d2162bb82bc5186
Daniel Javorszky <daniel.javorszky@gmail.com>
2023-12-04 22:57:08 +0100

    fix day 1 part 2

--- src/main/days/day1.clj
+++ src/main/days/day1.clj
@@ -23,7 +23,7 @@
   {"one" "1", "two" "2", "three" "3", "four" "4", "five" "5", "six" "6", "seven" "7", "eight" "8", "nine" "9",
    "oneight" "18", "twone" "21", "threeight" "38", "fiveight" "58", "sevenine" "79", "eightwo" "82", "eighthree" "83", "nineight" "98"})
 
-(def pattern #"(one|two|three|four|five|six|seven|eight|nine|oneight|twone|threeight|fiveight|sevenine|eightwo|eighthree|nineight)")
+(def pattern #"(oneight|twone|threeight|fiveight|sevenine|eightwo|eighthree|nineight|one|two|three|four|five|six|seven|eight|nine)")
 
 
 (defn task2 [input]
