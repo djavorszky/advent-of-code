68e737c83c8fc0cc079c20bb1844bcd957ff5394
Daniel Javorszky <daniel.javorszky@gmail.com>
2023-12-04 21:34:16 +0100

    day 4 part 2

--- src/main/days/day4.clj
+++ src/main/days/day4.clj
@@ -1,6 +1,6 @@
 (ns days.day4
   (:require [clojure.string :as str]))
-  
+
 (defn str->int [x] (Integer/valueOf x))
 
 (defn line->card [line]
@@ -14,9 +14,10 @@
   (->> (:cards card)
        (filter #(contains? (:winners card) %))))
 
+(defn winning-count [card] (count (winning-numbers card)))
+
 (defn card-value [card]
-  (let [winning-count (count (winning-numbers card))]
-    (int (Math/pow 2 (dec winning-count)))))
+  (int (Math/pow 2 (dec (winning-count card)))))
 
 (def input (slurp "input/4.txt"))
 (def lines (str/split-lines input))
@@ -28,7 +29,53 @@
        (map card-value)
        (reduce +)))
 
+
+
 (task1 lines)
+
+
+;; ---- part 2 -----
+
+(defn scratch-game [cards]
+  (->> cards
+       (map winning-count)
+       (map #(hash-map :wins % :has 1))
+       (into [])))
+
+(defn amount-won [card-num cards]
+  (:wins (nth cards (dec card-num))))
+
+
+(defn update-counts [card-num cards]
+  (let [amount (:has (nth cards (dec card-num)))]
+    (loop [idx card-num
+           max (+ (dec idx) (amount-won card-num cards))
+           cards cards]
+      (if (<= idx max)
+        (recur
+         (inc idx)
+         max
+         (update-in cards [idx :has] + amount))
+        cards))))
+
+(defn play-cards [cards]
+  (loop [cards (scratch-game cards)
+         idx 0
+         max (count cards)]
+    (if (< idx max)
+      (recur (update-counts (inc idx) cards) (inc idx) max)
+      cards)))
+
+
+
+(defn task2 [lines]
+  (->> lines
+       (map line->card)
+       (play-cards)
+       (map :has)
+       (reduce +)))
+
+(task2 lines)
 
 (comment
 
@@ -41,6 +88,8 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"))
 
   (def card (line->card (first test-input)))
 
+  card
+
   (->> (:cards card)
        (filter #(contains? (:winners card) %))
        (count))
@@ -49,17 +98,29 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"))
         winning-count (count (winning-numbers card))]
     (int (Math/pow 2 (dec winning-count))))
 
-  card
+
   (filter #(contains? (:winners card) %) (:cards card))
 
 
-  (->> test-input
-       (map line->card)
-       (map card-value)
+  (def test-cards (map line->card test-input))
+
+  (def scratchies (scratch-game test-cards))
+
+
+  (defn play-cards [cards]
+    (loop [cards (scratch-game cards)
+           idx 0
+           max (count cards)]
+      (if (< idx max)
+        (recur (update-counts (inc idx) cards) (inc idx) max)
+        cards)))
+
+  (->> (play-cards test-cards)
+       (map :has)
        (reduce +))
 
 
-  )
+  (def scratchies (scratch-game test-cards))
 
-
+  (update-in scratchies [0 :wins] inc))
 
